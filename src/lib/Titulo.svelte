3ecc62345cc1867433da5ce8f429ff1b18da4558
Angellomon <angelmtzdiaz@gmail.com>
2022-04-01 16:16:08 -0600

    feat(components/Titulo): implementación

---
+++ src/lib/Titulo.svelte
@@ -0,0 +1,42 @@
+<div>
+  <p class="quinto">5°</p>
+  <span>
+    <p class="ciclo">CICLO INTERNACIONAL DE</p>
+    <br />
+    <p class="conferencias">CONFERENCIAS</p>
+  </span>
+  <p class="a_2022">/ 2022</p>
+</div>
+
+<style>
+  div {
+    display: flex;
+    align-items: center;
+  }
+
+  span {
+    align-self: center;
+    margin-bottom: 0.6em;
+    margin-right: 0.4em;
+  }
+
+  p {
+    display: inline;
+  }
+
+  p.quinto {
+    font-size: 1.5em;
+  }
+
+  p.ciclo {
+    font-size: 0.6em;
+  }
+
+  p.conferencias {
+    font-size: 1em;
+  }
+
+  p.a_2022 {
+    font-size: 1.5em;
+  }
+</style>
