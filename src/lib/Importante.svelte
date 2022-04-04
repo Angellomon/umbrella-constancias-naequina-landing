2d5db4d7eebaf2146dde3286798c69095da91001
Angellomon <angelmtzdiaz@gmail.com>
2022-04-03 22:38:22 -0500

    feat(lib/Importante): implementacion de mnsaje importante para moble

---
+++ src/lib/Importante.svelte
@@ -0,0 +1,42 @@
+<script></script>
+
+<section>
+  <div>
+    <h2>IMPORTANTE</h2>
+    <p>Para descargar tu constancia, es necesario que hayas cumplido con el requerimiento mínimo de asistencia del 75% del tiempo.</p>
+    <p>En caso contrario, no podrás descargar la constancia.</p>
+  </div>
+</section>
+
+<style>
+
+  section {
+    background-color: var(--gris-principal);
+
+    display: flex;
+    justify-content: center;
+    align-content: center;
+
+  }
+  
+  div {
+    margin: 5vw;
+    padding: 0 5vw;
+
+    background-color: var(--naranja-principal);
+
+    border-radius: 35px;
+
+    text-align: center;
+    color: white;
+  }
+
+  p {
+    margin: 0.8em 0.5em;
+  }
+
+  h2 {
+    margin: 0.5em;
+  }
+
+</style>
