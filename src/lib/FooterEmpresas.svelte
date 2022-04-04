d9d9e4799d38e819a1183603d32059568e5a88a0
Angellomon <angelmtzdiaz@gmail.com>
2022-04-03 22:37:46 -0500

    feat(lib/footers): implementacion de footers para mobile

---
+++ src/lib/FooterEmpresas.svelte
@@ -0,0 +1,24 @@
+<footer>
+  <img src="/logos/galope.png" alt="GALOPE">
+  <img src="/logos/equitonik.png" alt="EQUITONIK">
+  <img src="/logos/galotec.png" alt="GALOTEC">
+  <img src="/logos/uvm.png" alt="UVM">
+</footer>
+
+<style>
+
+  img {
+    max-height: 3.2em;
+    height: 100%;
+    width: auto;
+  }
+
+  footer {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+
+    background-color: var(--gris-principal);
+  }
+
+</style>
---
+++ src/lib/FooterGrupoNutec.svelte
@@ -0,0 +1,74 @@
+<footer>
+  <img class="gruponutec" src="/logos/nutec.png" alt="GRUPONUTEC">
+  <span class="separator" />
+  <div>
+    <img class="icon" src="/icons/facebook.png" alt="facebook">
+    <img class="icon" src="/icons/youtube.png" alt="youtube">
+  </div>
+</footer>
+
+<style>
+  img.icon {
+    width: 40px;
+    height: 40px;
+    align-self: center;
+  }
+
+  img.gruponutec {
+    height: 60%;
+    width: 60%;
+    align-self: center;
+  }
+
+  div {
+    display: inline-flex;
+    border-right: none;
+    background-color: var(--azul-secundario);
+
+    padding-right: 2vw;
+  }
+
+  span.separator {
+    display: block;
+    position: relative;
+
+    width: 8.5vh;
+    height: 8vh;
+
+    transform: rotateY(180deg);
+
+    max-height: 10vh;
+  }
+
+  span.separator::before {
+    position: absolute;
+    content: "";
+    width: 100%;
+    height: 100%;
+    background-color: var(--azul-secundario);
+    clip-path: polygon(100% 0, 0 0 , 0 100%);
+  }
+  
+  span.separator::after {
+    position: absolute;
+    content: "";
+    width: 100%;
+    height: 100%;
+    background-color: var(--naranja-principal);
+    clip-path: polygon(100% 0, 0 100% , 100% 100%);
+  }
+
+  span.separator::before {
+    position: absolute;
+  }
+
+  footer {
+    background-color: var(--naranja-principal);
+
+    display: flex;
+    justify-content: right;
+
+    max-height: 10vh;
+  }
+
+</style>
