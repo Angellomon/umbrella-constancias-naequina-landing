375557ab346539f8dcd8861fe055c553079ba5ab
Angellomon <angelmtzdiaz@gmail.com>
2022-04-03 23:23:25 -0500

    feat(lib/solicita): implementacion de mensaje para mobile

---
+++ src/lib/SolicitaTuConstancia.svelte
@@ -0,0 +1,49 @@
+<script></script>
+
+<div>
+  <section>
+    <p>
+      <span class="black">SOLICITA TU</span>
+      <span class="white">CONSTANCIA</span>
+    </p>
+  </section>
+</div>
+
+<style>
+  div {
+
+    background: linear-gradient(90deg, rgba(0,0,0,0) 0%, #99cfff 100%);
+  }
+
+  section {
+    background-image: url("/img/caballo-transparente.png");
+    background-size: contain;
+    background-repeat: no-repeat;
+    border-top: 3px solid white;
+    border-bottom: 3px solid white;
+  }
+
+  p {
+    font-size: 2.6em;
+    font-weight: bold;
+
+    position: relative;
+    padding-right: 5%;
+
+    text-align: right;
+  }
+
+  span.black {
+    color: black;
+  }
+
+  span.white {
+    color: white;
+  }
+
+  @media (max-height: 411px) {
+    section {
+      height: 30vh;
+    }
+  }
+</style>
