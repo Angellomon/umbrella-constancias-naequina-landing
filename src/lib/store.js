9c38bb6ed7c8fb74351c5d589f32af5805533555
Angellomon <angelmtzdiaz@gmail.com>
2022-05-04 14:03:14 -0500

    feat(lib): descripcion de demás status

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,3 +1,11 @@
 import { writable } from "svelte/store";
 
 export const correo = writable("");
+
+export const isLoadingEmail = writable(false);
+
+export const status = writable({
+  downloadError: false,
+  invalidEmail: false,
+  loadingEmail: false,
+});
