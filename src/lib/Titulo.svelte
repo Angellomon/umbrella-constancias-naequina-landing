69940046a96393b0cc03e7fbd9a04ce38b671bc8
Angellomon <angelmtzdiaz@gmail.com>
2022-05-04 14:02:45 -0500

    feat(lib/Titulo): img

--- src/lib/Titulo.svelte
+++ src/lib/Titulo.svelte
@@ -1,11 +1,9 @@
+<script>
+  import LogoConferencia from "./LogoConferencia.svelte";
+</script>
+
 <div>
-  <p class="quinto">5°</p>
-  <span>
-    <p class="ciclo">CICLO INTERNACIONAL DE</p>
-    <br />
-    <p class="conferencias">CONFERENCIAS</p>
-  </span>
-  <p class="a_2022">/ 2022</p>
+  <LogoConferencia />
 </div>
 
 <style>
@@ -15,51 +13,9 @@
     justify-content: center;
   }
 
-  span {
-    align-self: center;
-    margin-bottom: 0.6em;
-    margin-right: 0.4em;
-  }
-
-  p {
-    display: inline;
-    margin: 0;
-
-    align-self: center;
-  }
-
-  p.quinto {
-    font-size: 1.5em;
-  }
-
-  p.ciclo {
-    font-size: 0.6em;
-    font-weight: bold;
-  }
-
-  p.conferencias {
-    font-size: 1em;
-  }
-
-  p.a_2022 {
-    font-size: 1.5em;
-  }
-
-  @media(min-width: 768px) {
-    p.quinto {
-      font-size: 2.4em;
-    }
-
-    p.ciclo {
-      font-size: 0.9em;
-    }
-
-    p.conferencias {
-      font-size: 1.5em;
-    }
-
-    p.a_2022 {
-      font-size: 2.4em;
+  @media (min-width: 768px) {
+    div {
+      margin-bottom: 10px;
     }
   }
 </style>
