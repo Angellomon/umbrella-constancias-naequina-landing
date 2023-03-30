f97573bc57250d16949e0d4c68c02187e8295d3d
Angellomon <angelmtzdiaz@gmail.com>
2023-03-30 12:17:36 -0600

    feat(lib/components): impl Header toggle

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,70 +1,85 @@
 <script lang="ts">
-  import { lang, content } from "../store"
+  import { lang, content } from "../store";
+  import { toggleLang } from "../utils";
 
-  export const target = "https://naequina.com/"
-
+  export const target = "https://naequina.com/";
 </script>
 
 <header>
-  <img class="logo" src={$content.LOGO_SRC[$lang]} alt="logo naequina esp">
+  <img class="logo" src={$content.LOGO_SRC[$lang]} alt="logo naequina esp" />
 
   <div class="contenido">
     <a href={target}>{$content.MAIN_SITE_BUTTON_TEXT[$lang]}</a>
-    <img class="flag" src={$content.FLAG_IMG_SRC[$lang]} alt="">
+    <img
+      on:click={() => toggleLang()}
+      on:keydown={() => {}}
+      class="flag"
+      src={$content.FLAG_IMG_SRC[$lang]}
+      alt={$lang}
+    />
   </div>
 </header>
 
 <style>
+  header {
+    background: var(--color-principal);
+    background: -moz-linear-gradient(
+      90deg,
+      var(--color-principal-rgba) 0%,
+      rgba(0, 0, 0, 1) 100%
+    );
+    background: -webkit-linear-gradient(
+      90deg,
+      var(--color-principal-rgba) 0%,
+      rgba(0, 0, 0, 1) 100%
+    );
+    background: linear-gradient(
+      90deg,
+      var(--color-principal-rgba) 0%,
+      rgba(0, 0, 0, 1) 100%
+    );
 
-header {
-  background: var(--color-principal);
-  background: -moz-linear-gradient(90deg, var(--color-principal-rgba) 0%, rgba(0,0,0,1) 100%);
-  background: -webkit-linear-gradient(90deg, var(--color-principal-rgba) 0%, rgba(0,0,0,1) 100%);
-  background: linear-gradient(90deg, var(--color-principal-rgba) 0%, rgba(0,0,0,1) 100%);
+    height: 10vh;
+    padding: 10px;
 
-  height: 10vh;
-  padding: 10px;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
 
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
+    justify-content: space-around;
+    align-items: center;
+  }
 
-  justify-content: space-around;
-  align-items: center;
-}
+  header img {
+    cursor: pointer;
+  }
 
-header img {
-  cursor: pointer;
-}
+  header img.logo {
+    max-width: 300px;
+  }
 
-header img.logo {
-  max-width: 300px;
-}
+  header img.flag {
+    max-width: 50px;
+    margin-left: 10px;
+  }
 
-header img.flag {
-  max-width: 50px;
-  margin-left: 10px;
-}
+  header a {
+    text-decoration: none;
+    color: var(--font-color);
 
-header a {
-  text-decoration: none;
-  color: var(--font-color);
+    padding: 10px 10px;
+  }
 
-  padding: 10px 10px;
-}
+  header a:hover {
+    background-color: rgba(255, 255, 255, 0.1);
+  }
 
-header a:hover{
+  div.contenido {
+    /* min-width: 45vw; */
 
-  background-color: rgba(255, 255, 255, 0.1);
-}
-
-div.contenido {
-  /* min-width: 45vw; */
-
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-}
-
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  }
 </style>
