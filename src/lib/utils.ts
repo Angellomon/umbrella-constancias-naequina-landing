c3f493674146e49ca4f40f2826ef3611f66af30a
Angellomon <angelmtzdiaz@gmail.com>
2023-03-30 18:33:58 -0600

    feat(lib/utils): impl email validation

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -12,3 +12,17 @@ export function toggleLang() {
     lang.set("en");
   }
 }
+
+export function submitEmail(email: string) {
+  const isValidEmail = validateEmail(email);
+
+  if (!isValidEmail) return;
+
+  // TODO: impl pdf download
+}
+
+export function validateEmail(email: any) {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
