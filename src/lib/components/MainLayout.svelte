b5adfdae0fc6d6fbcf3c3fbb17504eba6d0f6dc7
Angellomon <angelmtzdiaz@gmail.com>
2023-03-30 18:36:55 -0600

    feat(layout/main): impl Message

--- src/lib/components/MainLayout.svelte
+++ src/lib/components/MainLayout.svelte
@@ -1,7 +1,21 @@
 <script>
+  import Email from "./Email.svelte";
   import Header from "./Header.svelte";
   import Message from "./Message.svelte";
+  import Title from "./Title.svelte";
 </script>
 
 <Header />
-<Message />
+<Title />
+<div class="content">
+  <Email />
+  <Message />
+</div>
+
+<style>
+  div.content {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+  }
+</style>
